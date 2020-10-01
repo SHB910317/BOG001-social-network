bffd80323ea7090321ca0d80146aabdfb557388f
SHB910317 <hernandezbarrero@gmail.com>
2020-10-01 12:29:56 -0500

    Updates

--- test/wall.spec.js
+++
@@ -1,9 +0,0 @@
-import { Wall } from '../src/lib/views/wall';
-
-describe('createPost', () => {
-  it('Debería crear un post', () => {
-    return Wall('Testing #1').then((data) => {
-      expect(data).toBe('Testing #1');
-    });
-  });
-});
